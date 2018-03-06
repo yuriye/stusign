8b2e595ab376ca97fc7762c456d3a2ed674cfbe8
Yuriy Yeliseyev <yuriye@mail.ru>
2018-03-06 12:19:12 +1200

    180306.1

--- src/com/ys/SettingsReader.java
+++ src/com/ys/SettingsReader.java
@@ -5,12 +5,14 @@ import argo.jdom.JsonNode;
 import argo.jdom.JsonNodeType;
 import argo.jdom.JsonStringNode;
 import argo.saj.InvalidSyntaxException;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,23 +31,23 @@ public class SettingsReader {
 
             Iterator i$ = root.getFields().keySet().iterator();
 
-            while(true) {
+            while (true) {
                 JsonStringNode node;
                 do {
                     if (!i$.hasNext()) {
                         return;
                     }
 
-                    node = (JsonStringNode)i$.next();
-                } while(root.getNode(new Object[]{node.getText()}).getType() != JsonNodeType.STRING);
+                    node = (JsonStringNode) i$.next();
+                } while (root.getNode(new Object[]{node.getText()}).getType() != JsonNodeType.STRING);
 
                 if (!"controlPanelUuid".equals(node.getText()) && !"thisDeviceUuid".equals(node.getText())) {
-                    settings.put(node.getText(), ((JsonNode)root.getFields().get(node)).getText());
+                    settings.put(node.getText(), ((JsonNode) root.getFields().get(node)).getText());
                 } else {
-                    settings.put("deviceUuid", ((JsonNode)root.getFields().get(node)).getText());
+                    settings.put("deviceUuid", ((JsonNode) root.getFields().get(node)).getText());
                 }
 
-                log.info("load local setting \"{}\" : \"{}\"", node.getText(), ((JsonNode)root.getFields().get(node)).getText());
+                log.info("load local setting \"{}\" : \"{}\"", node.getText(), ((JsonNode) root.getFields().get(node)).getText());
             }
         } catch (IOException var4) {
             log.error(var4.getMessage(), var4);
@@ -71,7 +73,7 @@ public class SettingsReader {
     }
 
     public static String getParam(String param) {
-        return (String)settings.get(param);
+        return (String) settings.get(param);
     }
 
     public static void putParam(String paramName, String param) {
